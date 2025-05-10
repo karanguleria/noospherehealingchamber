0f882a33edf0dff61a4a305f03fce8e038fd54ab
Karan Singh <himekaranguleria@gmail.com>
2025-05-10 17:15:12 +0000

    added server changes

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -4,28 +4,28 @@
       <!-- Name -->
       <div>
          <x-input-label for="name" :value="__('Name')" />
-         <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required
+         <x-text-input id="name" class="form-control form-input form-control-bordered w-full" type="text" name="name" :value="old('name')" required
             autofocus autocomplete="name" />
          <x-input-error :messages="$errors->get('name')" class="mt-2" />
       </div>
       <!-- Email Address -->
       <div class="mt-4">
          <x-input-label for="email" :value="__('Email')" />
-         <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')"
+         <x-text-input id="email" class="form-control form-input form-control-bordered w-full" type="email" name="email" :value="old('email')"
             required autocomplete="username" />
          <x-input-error :messages="$errors->get('email')" class="mt-2" />
       </div>
       <!-- Password -->
       <div class="mt-4">
          <x-input-label for="password" :value="__('Password')" />
-         <x-text-input id="password" class="block mt-1 w-full" type="password" name="password" required
+         <x-text-input id="password" class="form-control form-input form-control-bordered w-full" type="password" name="password" required
             autocomplete="new-password" />
          <x-input-error :messages="$errors->get('password')" class="mt-2" />
       </div>
       <!-- Confirm Password -->
       <div class="mt-4">
          <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
-         <x-text-input id="password_confirmation" class="block mt-1 w-full" type="password"
+         <x-text-input id="password_confirmation" class="form-control form-input form-control-bordered w-full" type="password"
             name="password_confirmation" required autocomplete="new-password" />
          <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
       </div>
@@ -37,7 +37,7 @@
             href="{{ route('login') }}">
          {{ __('Already registered?') }}
          </a>
-         <x-primary-button class="ml-4">
+         <x-primary-button class="ml-4 btn-submit">
             {{ __('Register') }}
          </x-primary-button>
       </div>
--- resources/views/email/welcome.blade.php
+++ resources/views/email/welcome.blade.php
@@ -43,14 +43,23 @@
                 <!-- Main Email Container -->
                 <table class="wrapper" width="600" cellpadding="0" cellspacing="0" style="width:600px; max-width: 100%; margin: 0 auto;">
 
+                  <tr>
+                        <td style="background-color: #000035;" align="center">
+                            <img src="{{ asset('img/mail-header.jpg') }}" alt="Noosphere Healing Chamber" style="display:block; max-width:100%; height:auto;" />
+                        </td>
+                    </tr>
                     <!-- Main Content -->
                     <tr>
-                        <td class="content" style="background-color: #000035; padding: 20px; color:#ffffff; ">
-                        <h1 style="font-size: 14px;">Hi  {{ucfirst($user->first_name) ?? 'Practitioner'}},</h1>    
-                        <h2 style="color: #ffffff; margin-top: 0;font-size: 14px;">You’ve been added to the Noosphere Healing Chamber as a {{ $user->type_id == 2 ? 'Practitioner' : 'Client' }}</h2>
-                           
-                            <p style="color: #ffffff; line-height: 1.6; font-size: 12px; margin: 15px 0;">
-                                Here are your login details:</p>
+                         <td class="content" style="padding: 20px; color:#000000; background-color:white; ">
+                        <p style="color: #000000;font-size: 12px;">Hi  {{ucfirst($user->first_name) ?? 'Practitioner'}},</p>    
+                        <!-- <h2 style="color: #000000; margin-top: 0;font-size: 14px;">Get Started with Your Practitioner Account</h2> -->
+                            <p style="color: #000000; line-height: 1.6; font-size: 12px; margin: 15px 0;">
+                            You’ve been added to the Noosphere Healing Chamber as a {{ $user->type_id == 2 ? 'Practitioner' : 'Client' }}
+                            </p>
+                            <p style="color: #000000; line-height: 1.6; font-size: 12px; margin: 15px 0;">
+                                Here are your login details:
+                            </p>
+                                
                             <ul style="list-style:none">
                                 <li style="margin-bottom:8px;">Email: <a href="mailto:{{ $user->email ?? '' }}" style="color:#18b69b;">{{ $user->email ?? '' }}</a></li>
                                 <li style="margin-bottom:8px;">Temporary Password: <span style="color:#18b69b;">{{ $password ?? '' }}</span></li>
@@ -59,14 +68,34 @@
                                     Please update your password here: <span style="color:#18b69b;">{{ $resetPasswordLink ?? ''}}<span>
                                 </li>
 
+<<<<<<< Updated upstream
                             </ul>                            
                            
                             <p> 
                                 If you have any questions or need assistance, feel free to contact us at <a href="mailto:support@projectnoosphere.com">support@projectnoosphere.com</a>
+=======
+                            </ul>  
+                            
+                                <p style="margin-top: 30px;">
+                                  <a href="https://noospherehealingchamber.exponentialhealthcare.com/" c
+                                  lass="button" 
+                                  style="display:inline-flex; background-color:#18b69b; color:#ffffff; padding:12px 25px; text-decoration:none; border-radius:5px; font-size: 14px;">Log In Now</a>
+                            </p>
+                             If you have any questions or need assistance, feel free to contact us at <a style="color:#18b69b;" href="mailto:alumni@quantumuniversity.com">alumni@quantumuniversity.com</a>
+                            <br><br>
+                            Sincerely,<br>
+
+                            The Noosphere Healing Chamber Team
+>>>>>>> Stashed changes
                             </p>
                         </td>
                     </tr>
-
+                            <tr>
+                        <td align="center">
+                            <img src="{{ asset('img/mail-footer.jpg') }}" alt="Footer" style="display:block; max-width:100%; height:auto;" />
+                        </td>
+                    </tr>
+                    
                 </table>
 
             </td>
@@ -74,5 +103,7 @@
     </table>
 
 </body>
+
+
 
 </html>
