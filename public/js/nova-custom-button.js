1482ef45e21f3a28d250a1bac3c25d3f60948ded
Dhani Ram <dhani.ram@envigo.co.uk>
2025-05-10 23:29:21 +0530

    added button text issue

--- public/js/nova-custom-button.js
+++ public/js/nova-custom-button.js
@@ -2,13 +2,13 @@ document.addEventListener('DOMContentLoaded', function () {
     if (window.location.pathname === '/nova/resources/invitations/new') {
         const observer = new MutationObserver(() => {
             const btn = document.querySelector('[dusk="create-and-add-another-button"] span');
+            console.log("btn",btn);
             if (btn && btn.textContent.trim() === 'Create & Add Another') {
                 btn.textContent = 'Send & Invite Another';
-                observer.disconnect(); // Stop watching once updated
+                observer.disconnect(); // Done watching
             }
         });
 
-        // Observe changes in the document body
         observer.observe(document.body, {
             childList: true,
             subtree: true
